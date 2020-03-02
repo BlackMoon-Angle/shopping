12278e121018e13d6a1b99a7db77702e26398799
BlackMoon-Angle <2717773566@qq.com>
2020-03-02 20:49:56 +0800

    第三天

--- gulpfile.js
+++ gulpfile.js
@@ -78,7 +78,7 @@ const delHandler = () => {
 const serverHandler = () => {
   return gulp.src('./dist') // 找到我要打开的页面的文件夹, 把这个文件夹当作网站根目录
              .pipe(webserver({ // 需要一些配置项
-               host: 'www.guoxiang.com', // 域名, 这个域名可以自定义
+               host: 'localhost', // 域名, 这个域名可以自定义
                port: 8080, // 端口号, 0 ~ 65535, 尽量不适用 0 ~ 1023
                open: './pages/index.html', // 你默认打开的首页, 从 dist 下面的目录开始书写
                livereload: true, // 自动刷新浏览器 - 热重启
@@ -93,7 +93,22 @@ const serverHandler = () => {
                  {
                    source: '/gx2',
                    target: 'http://127.0.0.1/xxx.php'
-                 }
+                 },
+                 {
+                   //首页头部导航，公告数据请求代理
+                   source:'/headerInfo',
+                   target:'http://www.okbuy.com/login/headerInfo/0.6793523287138559'
+                 },
+                 {
+                   //第一个商品列表，数据请求
+                   source:'/shopList',
+                   target:'http://www.okbuy.com/ajax/homepage/index'
+                 },
+                 {
+                  //第二个商品列表，数据请求
+                  source:'/shopList2',
+                  target:'http://www.okbuy.com/ajax/homepage/index'
+                }
                ]
              })) // 开启服务器
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,119 @@
-window.onload = function(){
+window.onload = function () {
+    //公告二级菜单渲染
+    $.ajax({
+        url: '/headerInfo',
+        data: {},
+        cache: false,
+        dataType: 'json',
+        success: function (res) {
+            $('#head_nav_div_ajax').html(res.notice.notice_list);
+        }
+    })
 
+    //第一商品列表渲染
+    $.ajax({
+        url: '/shopList',
+        data: { cid: '3' },
+        cache: false,
+        dataType: 'json',
+        success: function (res2) {
+
+            function shopTime() {
+                let str = '';
+                res2.res.forEach(item => {
+
+                    var et = new Date(item.endtime);
+                    var nt = new Date();
+                    var ct = parseInt((et.getTime() - nt.getTime()) / 1000);//时间差
+                    var day = parseInt(ct / (60 * 60 * 24));//天
+                    var hour = parseInt(ct / (60 * 60) % 24);//时
+                    var min = parseInt(ct / 60 % 60);//分
+                    var s = parseInt(ct % 60);//秒
+
+                    str += `
+                   <li>
+                        <a href="${item.link}" target="_blank">
+                            <img src="${item.img}" alt="">
+                            <span class="tit1">${item.title}</span>
+                            <span class="tit2">${item.slogan}</span>
+                            <span class="tit3">${item.discount}</span>
+                            <span class="time" id="timetwo0">
+                                <span class="day"><i>${day}</i>天</span>
+                                <span class="hour"><i>${hour}</i>时</span>
+                                <span class="minute"><i>${min}</i>分</span>
+                                <span class="second"><i>${s}</i>秒</span>
+                            </span>        
+                        </a>
+                    </li>
+                   `
+                })
+                $('#list2in_ul').html(str);
+            }
+            shopTime();
+            setInterval(shopTime, 1000);
+        }
+    })
+
+    //第二列商品渲染
+    $.ajax({
+        url: '/shopList2',
+        data: { cid: '4' },
+        cache: false,
+        dataType: 'json',
+        success: function (res3) {
+            
+            function shopTime2(){
+                let str = '';
+
+                res3.res.forEach(item => {
+
+                    var et = new Date(item.endtime);
+                    var nt = new Date();
+                    var ct = parseInt((et.getTime() - nt.getTime()) / 1000);//时间差
+                    var day = parseInt(ct / (60 * 60 * 24));//天
+                    var hour = parseInt(ct / (60 * 60) % 24);//时
+                    var min = parseInt(ct / 60 % 60);//分
+                    var s = parseInt(ct % 60);//秒
+                    str += `
+                    <li>
+                        <a href="${item.link}" target="_blank">
+                            <img src="${item.img}">
+                            <p class="list3tit1">${item.title}
+                                <span>${item.discount}</span>
+                            </p>
+                            <span class="time2" id="timethree0">
+                                <span class="day">
+                                    <i>${day}</i>天
+                                </span>
+                                <span class="hour">
+                                    <i>${hour}</i>时
+                                </span>
+                                <span class="minute">
+                                    <i>${min}</i>分
+                                </span>
+                                <span class="second">
+                                    <i>${s}</i>秒
+                                </span>
+                            </span>
+                        </a>
+                    </li>
+                   `
+                })
+                $('#list3in_ul').html(str);
+            }
+            shopTime2();
+            setInterval(shopTime2, 1000);
+        }
+    })
 }
+
+//定时器倒计时
+// function shopTime(times) {
+//     var et = new Date(times);
+//     var nt = new Date();
+//     var ct = parseInt((et.getTime() - nt.getTime()) / 1000);//时间差
+//     var day = parseInt(ct / (60 * 60 * 24));//天
+//     var hour = parseInt(ct / (60 * 60) % 24);//时
+//     var min = parseInt(ct / 60 % 60);//分
+//     var s = parseInt(ct % 60);//秒
+// }
